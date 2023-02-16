de459a55bd4110a9317e6e5841d69d924750920c
cniscoding <asdfa.com>
2023-02-16 15:55:08 -0800

    lint and working. edge cases not applied

--- fetcher.js
+++ fetcher.js
@@ -6,16 +6,20 @@ const args = process.argv.slice(2);
 const url = args[0]; // first argurment of command line states URL
 const saveLocation = args[1];
 
-const content = request(url, (error, response, body) => {  
-  const wordCount = body.length
+const content = request(url, (error, response, body) => {
+  const wordCount = body.length;
+  // if(error){
+  //   console.log('invalid url please try again')
+  //   return;
+  // }
   // console.log('body:', body); // Print the HTML body for webpage
   fs.writeFile(saveLocation, body, err => {
     if (err) {
       console.error(err);
     }
-    if (saveLocation){
-      console.log('file locaiton already exist')
-      return
+    if (saveLocation) {
+      console.log('file locaiton already exist');
+      return;
     }
     // file written successfully
     console.log(`Downloaded and saved ${wordCount} bytes to ${url}`);
