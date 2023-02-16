15b047aee09540bc9dc3abd23ea3ac544fe4bb09
cniscoding <asdfa.com>
2023-02-16 15:49:25 -0800

    not working yet but have the bones

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,22 @@
 const request = require('request');
-request('https://www.google.com/asfkldjfaskdhjfsjkladh', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+
+const url = args[0]; // first argurment of command line states URL
+const saveLocation = args[1];
+
+const content = request(url, (error, response, body) => {  
+  const wordCount = body.length
+  // console.log('body:', body); // Print the HTML body for webpage
+  fs.writeFile(saveLocation, body, err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+    console.log(`Downloaded and saved ${wordCount} bytes to ${url}`);
+  });
 });
+
+
+
